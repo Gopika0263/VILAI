4f36626742c10277448f5f7502edd23a9011cea9
Gopika K <gopikakandhan63@gmail.com>
2026-03-24 20:30:56 +0530

    Update .gitignore to ignore secrets

---
+++ nilai_app/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.24.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\NILAI\\nilai_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.24.3-stable\\flutter"
+  "PROJECT_DIR=D:\\NILAI\\nilai_app"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.24.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\NILAI\\nilai_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\NILAI\\nilai_app"
+  "FLUTTER_TARGET=D:\\NILAI\\nilai_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\NILAI\\nilai_app\\.dart_tool\\package_config.json"
+)
